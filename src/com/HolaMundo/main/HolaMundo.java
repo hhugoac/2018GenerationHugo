9971467b5fb19aae843afe3ba80e0070e7202d21
hhugoac <hhugoac@gmail.com>
2018-06-12 11:48:32 -0500

    Primera versión del proyecto

---
+++ src/com/HolaMundo/main/HolaMundo.java
@@ -0,0 +1,13 @@
+package com.HolaMundo.main;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo en gitHub");
+		System.out.println("Hola mundo en gitHub22");
+		System.out.println("Hola mundo en gitHub3");
+
+	}
+
+}
